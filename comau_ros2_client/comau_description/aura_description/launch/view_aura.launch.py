a63d484eee2f1b18a4415d4e592bbf20aede52dd
giulia <giulia.nicastro@external.comau.com>
2024-03-20 10:26:51 +0100

    launch.py file to load the robot model

---
+++ comau_ros2_client/comau_description/aura_description/launch/view_aura.launch.py
@@ -0,0 +1,50 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    xacro_file = os.path.join(get_package_share_directory('aura_description'), 'robots', 'aura_robot.urdf.xacro')    
+    assert os.path.exists(xacro_file), "The aura_robot.urdf.xacro doesnt exist in "+str(xacro_file)
+
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_desc = robot_description_config.toxml()
+
+    #print(robot_desc)
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[
+                {"robot_description": robot_desc}],
+            output="screen"),
+            
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[
+                {"robot_description": robot_desc}],
+          ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen')
+            
+    #    Node(
+    #        package='joint_state_publisher_gui',
+    #        executable='joint_state_publisher_gui',
+    #        name='joint_state_publisher_gui',
+    #      )
+    ])
