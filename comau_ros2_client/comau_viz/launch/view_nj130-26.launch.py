cee64f74fed081eed6e8010b20a51a916076eaf9
giulia <giulia.nicastro@external.comau.com>
2024-03-21 11:47:10 +0100

    porting comau_viz package from ros1 to ros2

---
+++ comau_ros2_client/comau_viz/launch/view_nj130-26.launch.py
@@ -0,0 +1,42 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    xacro_file = os.path.join(get_package_share_directory('nj130-26_description'), 'robots', 'nj130-26_robot.urdf.xacro')    
+    assert os.path.exists(xacro_file), "The nj130-26_robot.urdf.xacro doesnt exist in "+str(xacro_file)
+
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_desc = robot_description_config.toxml()
+
+    #print(robot_desc)
+
+    return LaunchDescription([
+      Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[
+            {"robot_description": robot_desc}],
+        output="screen"),
+          
+      Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[
+          {"robot_description": robot_desc}]),
+      Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen')
+        
+    #    Node(
+    #        package='joint_state_publisher_gui',
+    #        executable='joint_state_publisher_gui',
+    #        name='joint_state_publisher_gui',
+    #      )
+  ])
