19777a3e99e428ff6c2f18633ac9a78b3ca018da
giulia <giulia.nicastro@external.comau.com>
2024-06-04 12:13:39 +0200

    added nj220 and nj4-170-29

--- comau_ros2_client/test/src/joint_limits_test.py
+++ comau_ros2_client/test/src/joint_limits_test.py
@@ -66,67 +66,142 @@ class YourTestClass(unittest.TestCase):
             }
           },
 
-           {       
-              "robot_name": "nj4-170-29",
-              "urdf_file_path": os.path.join(get_package_share_directory('nj4-170-29_description'), 'urdf', 'nj4-170-29', 'nj4-170-29_robot.urdf'),
-              "expected_joint_count": 14,
-              "expected_joints": [
-                "joint_1",
-                "joint_2",
-                "joint_2m",
-                "joint_3",
-                "joint_4",
-                "joint_5",
-                "joint_6",
-                "fixed_ee_joint",
-                "world_joint", 
-              # joints: parallel bar joints
-                "joint_2_1",
-                "joint_2_2"  , 
-                "joint_2_2_5" ,  
-                "joint_3_1"  ,  
-                "joint_3_1_5" ,  
-              ],
-              "expected_joint_limits": {             
-                "joint_1":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 1.75},
-                "joint_2":  {'lower': -0.34907, 'upper': 1.4835,   'effort': 70,  'velocity': 1.48},   
-                "joint_2m": {'lower': -3.8397,  'upper': -0.87267, 'effort': 70,  'velocity': 1.48},   
-                "joint_3":  {'lower': -3.8397,  'upper': -0.87267, 'effort': 100, 'velocity': 1.75},
-                "joint_4":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 2.27},
-                "joint_5":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 2.44},
-                "joint_6":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 70,  'velocity': 3.32},
-              }
-           },
+          {       
+            "robot_name": "nj4-170-29",
+            "urdf_file_path": os.path.join(get_package_share_directory('nj4-170-29_description'), 'urdf', 'nj4-170-29', 'nj4-170-29_robot.urdf'),
+            "expected_joint_count": 14,
+            "expected_joints": [
+              "joint_1",
+              "joint_2",
+              "joint_2m",
+              "joint_3",
+              "joint_4",
+              "joint_5",
+              "joint_6",
+              "fixed_ee_joint",
+              "world_joint", 
+            # joints: parallel bar joints
+              "joint_2_1",
+              "joint_2_2"  , 
+              "joint_2_2_5" ,  
+              "joint_3_1"  ,  
+              "joint_3_1_5" ,  
+            ],
+            "expected_joint_limits": {             
+              "joint_1":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 1.75},
+              "joint_2":  {'lower': -0.34907, 'upper': 1.4835,   'effort': 70,  'velocity': 1.48},   
+              "joint_2m": {'lower': -3.8397,  'upper': -0.87267, 'effort': 70,  'velocity': 1.48},   
+              "joint_3":  {'lower': -3.8397,  'upper': -0.87267, 'effort': 100, 'velocity': 1.75},
+              "joint_4":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 2.27},
+              "joint_5":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 100, 'velocity': 2.44},
+              "joint_6":  {'lower': -3.1416,  'upper': 3.1416,   'effort': 70,  'velocity': 3.32},
+            }
+          },
 
-           {       
-              "robot_name": "racer5-0-80",
-              "urdf_file_path": os.path.join(get_package_share_directory('racer5-0-80_description'), 'urdf', 'racer5-0-80', 'racer5-0-80_robot.urdf'),
-              "expected_joint_count": 8,
-              "expected_joints": [
-                "joint_1",
-                "joint_2",
-                "joint_3",
-                "joint_4",
-                "joint_5",
-                "joint_6",
-                "joint_ee_link",
-                "world_joint", 
-              ],
-              "expected_joint_limits": {             
+          {       
+             "robot_name": "nj220",
+             "urdf_file_path": os.path.join(get_package_share_directory('nj220_description'), 'urdf', 'nj220', 'nj220_robot.urdf'),
+             "expected_joint_count": 8,
+             "expected_joints": [
+               "joint_1",
+               "joint_2",
+               "joint_3",
+               "joint_4",
+               "joint_5",
+               "joint_6",
+               "joint_ee_link",
+               "world_joint",  
+             ],
+             "expected_joint_limits": {             
+               "joint_1": {'lower': -3.14,  'upper': 3.14,  'effort': 10000.00, 'velocity': 20.0},
+               "joint_2": {'lower': -1.309, 'upper': 1.658, 'effort': 10000.00, 'velocity': 20.0},   
+               "joint_3": {'lower': -1.396, 'upper': 1.238, 'effort': 10000.00, 'velocity': 20.0},   
+               "joint_4": {'lower': -3.14,  'upper': 3.14, 'effort': 10000.00, 'velocity': 20.0},
+               "joint_5": {'lower': -2.182, 'upper': 2.182, 'effort': 10000.00, 'velocity': 20.0},
+               "joint_6": {'lower': -3.14,  'upper': 3.14,  'effort': 10000.00, 'velocity': 20.0},
+             }
+          },           
+
+          {       
+             "robot_name": "racer5-0-80",
+             "urdf_file_path": os.path.join(get_package_share_directory('racer5-0-80_description'), 'urdf', 'racer5-0-80', 'racer5-0-80_robot.urdf'),
+             "expected_joint_count": 8,
+             "expected_joints": [
+               "joint_1",
+               "joint_2",
+               "joint_3",
+               "joint_4",
+               "joint_5",
+               "joint_6",
+               "joint_ee_link",
+               "world_joint", 
+             ],
+             "expected_joint_limits": {             
+               "joint_1": {'lower': -2.966, 'upper': 2.966, 'effort': 100000.00, 'velocity': 20.00},
+               "joint_2": {'lower': -1.99,  'upper': 1.116, 'effort': 100000.00, 'velocity': 20.00},    
+               "joint_3": {'lower': -2.62,  'upper': 1.483, 'effort': 100000.00, 'velocity': 20.00},
+               "joint_4": {'lower': -3.49,  'upper': 3.49,  'effort': 100000.00, 'velocity': 20.00},
+               "joint_5": {'lower': -1.83,  'upper': 1.83,  'effort': 100000.00, 'velocity': 20.00},
+               "joint_6": {'lower': -47.10, 'upper': 47.10, 'effort': 100000.00, 'velocity': 20.00},
+             }
+          },  
+
+          {       
+             "robot_name": "racer5-cobot",
+             "urdf_file_path": os.path.join(get_package_share_directory('racer5-cobot_description'), 'urdf', 'racer5-cobot', 'racer5-cobot_robot.urdf'),
+             "expected_joint_count": 8,
+             "expected_joints": [
+               "joint_1",
+               "joint_2",
+               "joint_3",
+               "joint_4",
+               "joint_5",
+               "joint_6",
+               "joint_ee_link",
+               "world_joint", 
+             ],
+             "expected_joint_limits": {             
                 "joint_1": {'lower': -2.966, 'upper': 2.966, 'effort': 100000.00, 'velocity': 20.00},
-                "joint_2": {'lower': -1.99,  'upper': 1.116, 'effort': 100000.00, 'velocity': 20.00},    
+                "joint_2": {'lower': -1.116, 'upper': 1.99,  'effort': 100000.00, 'velocity': 20.00},    
                 "joint_3": {'lower': -2.62,  'upper': 1.483, 'effort': 100000.00, 'velocity': 20.00},
                 "joint_4": {'lower': -3.49,  'upper': 3.49,  'effort': 100000.00, 'velocity': 20.00},
                 "joint_5": {'lower': -1.83,  'upper': 1.83,  'effort': 100000.00, 'velocity': 20.00},
-                "joint_6": {'lower': -47.10, 'upper': 47.10, 'effort': 100000.00, 'velocity': 20.00},
-              }
-           },  
+                "joint_6": {'lower': -100,   'upper': 100,   'effort': 100000.00, 'velocity': 20.00},
+             }
+          },           
 
-           {       
-              "robot_name": "racer5-cobot",
-              "urdf_file_path": os.path.join(get_package_share_directory('racer5-cobot_description'), 'urdf', 'racer5-cobot', 'racer5-cobot_robot.urdf'),
-              "expected_joint_count": 8,
-              "expected_joints": [
+          {       
+             "robot_name": "racer5-cobot-rail",
+             "urdf_file_path": os.path.join(get_package_share_directory('racer5-cobot-rail_description'), 'urdf', 'racer5-cobot', 'racer5-cobot-rail_robot.urdf'),
+             "expected_joint_count": 10,
+             "expected_joints": [
+                "joint_1",
+                "joint_2",
+                "joint_3",
+                "joint_4",
+                "joint_5",
+                "joint_6",
+                "joint_7",
+                "rail_joint",
+                "joint_ee_link",
+                "world_joint", 
+             ],
+             "expected_joint_limits": {             
+                "joint_1": {'lower': -2.966, 'upper': 2.966, 'effort': 100000.00, 'velocity': 20.00},
+                "joint_2": {'lower': -1.116, 'upper': 1.99,  'effort': 100000.00, 'velocity': 20.00},    
+                "joint_3": {'lower': -2.62,  'upper': 1.483, 'effort': 100000.00, 'velocity': 20.00},
+                "joint_4": {'lower': -3.49,  'upper': 3.49,  'effort': 100000.00, 'velocity': 20.00},
+                "joint_5": {'lower': -1.83,  'upper': 1.83,  'effort': 100000.00, 'velocity': 20.00},
+                "joint_6": {'lower': -3.14,  'upper': 3.14,  'effort': 100000.00, 'velocity': 20.00},
+                "joint_7": {'lower': -0.0,   'upper': 2.0,   'effort': 100000.00, 'velocity': 20.00},             
+             }                
+          }, 
+
+          {       
+             "robot_name": "racer7-14",
+             "urdf_file_path": os.path.join(get_package_share_directory('racer7-14_description'), 'urdf', 'racer7-14', 'racer7-14_robot.urdf'),
+             "expected_joint_count": 8,
+             "expected_joints": [
                 "joint_1",
                 "joint_2",
                 "joint_3",
@@ -135,67 +210,16 @@ class YourTestClass(unittest.TestCase):
                 "joint_6",
                 "joint_ee_link",
                 "world_joint", 
-              ],
-              "expected_joint_limits": {             
-                 "joint_1": {'lower': -2.966, 'upper': 2.966, 'effort': 100000.00, 'velocity': 20.00},
-                 "joint_2": {'lower': -1.116, 'upper': 1.99,  'effort': 100000.00, 'velocity': 20.00},    
-                 "joint_3": {'lower': -2.62,  'upper': 1.483, 'effort': 100000.00, 'velocity': 20.00},
-                 "joint_4": {'lower': -3.49,  'upper': 3.49,  'effort': 100000.00, 'velocity': 20.00},
-                 "joint_5": {'lower': -1.83,  'upper': 1.83,  'effort': 100000.00, 'velocity': 20.00},
-                 "joint_6": {'lower': -100,   'upper': 100,   'effort': 100000.00, 'velocity': 20.00},
-              }
-           },           
-
-           {       
-              "robot_name": "racer5-cobot-rail",
-              "urdf_file_path": os.path.join(get_package_share_directory('racer5-cobot-rail_description'), 'urdf', 'racer5-cobot', 'racer5-cobot-rail_robot.urdf'),
-              "expected_joint_count": 10,
-              "expected_joints": [
-                 "joint_1",
-                 "joint_2",
-                 "joint_3",
-                 "joint_4",
-                 "joint_5",
-                 "joint_6",
-                 "joint_7",
-                 "rail_joint",
-                 "joint_ee_link",
-                 "world_joint", 
-              ],
-              "expected_joint_limits": {             
-                 "joint_1": {'lower': -2.966, 'upper': 2.966, 'effort': 100000.00, 'velocity': 20.00},
-                 "joint_2": {'lower': -1.116, 'upper': 1.99,  'effort': 100000.00, 'velocity': 20.00},    
-                 "joint_3": {'lower': -2.62,  'upper': 1.483, 'effort': 100000.00, 'velocity': 20.00},
-                 "joint_4": {'lower': -3.49,  'upper': 3.49,  'effort': 100000.00, 'velocity': 20.00},
-                 "joint_5": {'lower': -1.83,  'upper': 1.83,  'effort': 100000.00, 'velocity': 20.00},
-                 "joint_6": {'lower': -3.14,  'upper': 3.14,  'effort': 100000.00, 'velocity': 20.00},
-                 "joint_7": {'lower': -0.0,   'upper': 2.0,   'effort': 100000.00, 'velocity': 20.00},             
-              }                
-           }, 
-
-           {       
-              "robot_name": "racer7-14",
-              "urdf_file_path": os.path.join(get_package_share_directory('racer7-14_description'), 'urdf', 'racer7-14', 'racer7-14_robot.urdf'),
-              "expected_joint_count": 8,
-              "expected_joints": [
-                 "joint_1",
-                 "joint_2",
-                 "joint_3",
-                 "joint_4",
-                 "joint_5",
-                 "joint_6",
-                 "joint_ee_link",
-                 "world_joint", 
-              ],
-              "expected_joint_limits": {             
-                 "joint_1": {'lower': -2.8797932658, 'upper': 2.8797932658, 'effort': 10000.00, 'velocity': 20.00},
-                 "joint_2": {'lower': -1.4835298642, 'upper': 2.7052603406, 'effort': 10000.00, 'velocity': 20.00},    
-                 "joint_3": {'lower': -2.9321531434, 'upper': 0.0,          'effort': 10000.00, 'velocity': 20.00},
-                 "joint_4": {'lower': -3.6651914292, 'upper': 3.6651914292, 'effort': 10000.00, 'velocity': 20.00},
-                 "joint_5": {'lower': -2.3561944902, 'upper': 2.3561944902, 'effort': 10000.00, 'velocity': 20.00},
-                 "joint_6": {'lower': -47.1238898,   'upper': 47.1238898,   'effort': 10000.00, 'velocity': 20.00},
-              }                
-           }, 
+             ],
+             "expected_joint_limits": {             
+                "joint_1": {'lower': -2.8797932658, 'upper': 2.8797932658, 'effort': 10000.00, 'velocity': 20.00},
+                "joint_2": {'lower': -1.4835298642, 'upper': 2.7052603406, 'effort': 10000.00, 'velocity': 20.00},    
+                "joint_3": {'lower': -2.9321531434, 'upper': 0.0,          'effort': 10000.00, 'velocity': 20.00},
+                "joint_4": {'lower': -3.6651914292, 'upper': 3.6651914292, 'effort': 10000.00, 'velocity': 20.00},
+                "joint_5": {'lower': -2.3561944902, 'upper': 2.3561944902, 'effort': 10000.00, 'velocity': 20.00},
+                "joint_6": {'lower': -47.1238898,   'upper': 47.1238898,   'effort': 10000.00, 'velocity': 20.00},
+             }                
+          }, 
       ]
       
       # Inizializzazione liste
@@ -217,7 +241,7 @@ class YourTestClass(unittest.TestCase):
               self.assertEqual(len(found_joints), robot_info["expected_joint_count"],
                                f"Il NUMERO di giunti trovati nel file URDF per il robot {robot_info['robot_name']} non corrisponde al numero atteso di giunti.")
           except AssertionError as error:
-              print(f"Test fallito per il robot {robot_info['robot_name']}: {error}")
+              print(f"  *****     TEST FALLITO per il robot {robot_info['robot_name']}: {error}    *****")
               all_tests_passed = False 
 
           # Verifica che ciascun giunto atteso sia presente nei giunti trovati
@@ -258,7 +282,7 @@ class YourTestClass(unittest.TestCase):
                     #self.passed_robots_joint_limits.append(robot_info["robot_name"])
                   
                   except AssertionError as error:
-                    print(f"Test fallito per il robot {robot_info['robot_name']} - joint {joint.name}: {error}")
+                    print(f" *****     TEST FALLITO  per il robot {robot_info['robot_name']} - joint {joint.name}: {error}")
                     all_joints_passed = False
                     self.not_passed_robots_joint_limits.append(robot_info["robot_name"])
                   
