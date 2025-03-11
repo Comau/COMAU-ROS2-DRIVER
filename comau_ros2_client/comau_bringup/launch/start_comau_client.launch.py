95e9627a5b84cd2f3e55c42e0d37e9a4988de179
AndyP5 <andrea.perica@external.comau.com>
2025-03-11 16:39:12 +0100

    added racer3 description

--- comau_ros2_client/comau_bringup/launch/start_comau_client.launch.py
+++ comau_ros2_client/comau_bringup/launch/start_comau_client.launch.py
@@ -105,6 +105,18 @@ def generate_launch_description():
                 ]),
             )
         ])
+        case "racer3":
+            return LaunchDescription([
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    PathJoinSubstitution([
+                        FindPackageShare('racer3_0_63_description'),
+                        'launch/',
+                        'view_racer3_0_63.launch.py'
+                    ])
+                ]),
+            )
+        ])
         case "racer5-0-80":
             return LaunchDescription([
             IncludeLaunchDescription(
@@ -154,7 +166,7 @@ def generate_launch_description():
             )
         ])
         case _:
-            print("Client closed. Please choose a robot_type:= \n aura \n aura-mimic \n nj4-110 \n nj-165-30 \n nj4-170-29 \n nj130-26 \n nj220 \n racer5-0-80 \n racer5-cobot \n racer5-cobot-rail \n racer7-14")
+            print("Client closed. Please choose a robot_type:= \n aura \n aura-mimic \n nj4-110 \n nj-165-30 \n nj4-170-29 \n nj130-26 \n nj220 \n racer3 \n racer5-0-80 \n racer5-cobot \n racer5-cobot-rail \n racer7-14")
             sys.exit()
     
     return
